e0b085ed4e829af1eda4d8ef3cb798dc9c9d0adf
O Moura <123185795+LucRevoltz207@users.noreply.github.com>
2024-03-27 00:13:26 -0300

    Atualizar o 01-intro-python
    Exercícios moodle

---
+++ 01-intro-python/ex2.py
@@ -0,0 +1,7 @@
+numero1 = int(input ("digite o primeiro numero\n"))
+numero2 = int(input ("digite o segundo numero\n"))
+numero3 = int(input ("digite o terceiro numero\n"))
+
+media = (numero1 + numero2 + numero3) / 3
+
+print("a média aritmética é: ", media)
