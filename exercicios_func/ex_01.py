57c464040f39fb360ccdd3a1d6fa9377a3bc7f27
LucRevoltz207 <l.conceicao@aluno.ifsp.edu.br>
2024-04-09 09:17:25 -0300

    exercicios

---
+++ exercicios_func/ex_01.py
@@ -0,0 +1,23 @@
+''' Jogo de Adivinhação: Peça ao usuário para adivinhar um número entre 1 e 100 que o programa escolheu aleatoriamente.
+ Informe ao usuário se o palpite está alto ou baixo, até que ele acerte o número.'''
+
+ 
+import random
+
+num = random.randint(1,100)
+print(num)
+ent = int(input())
+def retorna_se_maior(ent,num):
+    if ent >100 or ent <1:
+        return f'fora da faixa'
+    elif ent < num:
+        return f'Tá alto fio'
+    elif ent > num:
+        return f'Ta baixo fio'
+    while ent!= num:
+        while ent!= num:
+            print(retorna_se_maior(ent= ent, num= num))
+            
+        
+
+print("Acertou miseravi")
