70b5f02bbc4b26c986fbb77cecb4ccde588cf737
O Moura <123185795+LucRevoltz207@users.noreply.github.com>
2024-04-17 00:04:34 -0300

    Update ex_07.py

--- exercicios_func/ex_07.py
+++ exercicios_func/ex_07.py
@@ -1,15 +1,10 @@
-'''Ex07 - Jogo da Forca: Implemente uma versão simples do jogo da forca.
-O programa começa com uma palavra oculta (o usuário não vê) e o
-usuário tenta adivinhar a palavra, letra por letra. O usuário tem
- um número limitado de tentativas para adivinhar toda a palavra.'''
-
 '''
 Ex07 - Jogo da Forca: Implemente uma versão simples do jogo da forca. 
 O programa começa com uma palavra oculta (o usuário não vê) e o usuário tenta adivinhar a palavra, 
 letra por letra. 
 O usuário tem um número limitado de tentativas para adivinhar toda a palavra.
 '''
-# nao to conseguindo tirara o numero de chances conforme o usuário joga
+
 
 def forca(palavra):
     
